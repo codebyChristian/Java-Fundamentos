5d25ff840f8a44460f19b5baeee631e762f2e8fa
codebyChristian <Christ.Cardoso.santos@gmail.com>
2023-12-27 15:52:46 -0300

    Primeiro commit

---
+++ PrimeiroPrograma/src/Primeiro/Principal.java
@@ -0,0 +1,45 @@
+package Primeiro;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		int idade = 10;
+		String nome = "Christian";
+		idade = idade +2;
+		double peso = 72.5;
+		
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Digite idade, peso e nome: ");
+		idade = teclado.nextInt();
+		peso = teclado.nextDouble();
+		nome = teclado.next();
+		
+		System.out.println();
+		
+		System.out.println("Nome:" + nome);
+		System.out.printf("Idade: %d,\n", idade);
+		System.out.printf("Peso: %.2f\n",peso);
+		
+		
+		System.out.println("Idade: " + idade);
+		System.out.printf("Idade: %d,\n", idade);
+		System.out.printf("Peso: %.2f\n",peso);
+		System.out.println("Ola Mundo! ");
+		System.out.println("Hello Word!");	
+		
+		if(idade <18) {
+			System.out.println("Criança");
+		}
+		else if(idade <65) {
+			System.out.println("Idoso");
+		}
+		else {
+			System.out.println("Muito Idoso");
+		}
+				
+	}	
+		
+}
