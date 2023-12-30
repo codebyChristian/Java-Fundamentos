bd4cdb9a17e929f31aa24dfc17a718f2eb5a2cba
codebyChristian <Christ.Cardoso.santos@gmail.com>
2023-12-30 13:54:13 -0300

    Somado números: fundamentos

--- Calculadora_fundamentos/src/com/codeBychristian/Calcular.java
+++ Calculadora_fundamentos/src/com/codeBychristian/Calcular.java
@@ -7,18 +7,20 @@ public class Calcular {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Soma de números");
+		
 		Scanner number1 = new Scanner(System.in);
 		System.out.println("Digite o primeiro número: ");
 		int numero1 = Integer.parseInt(number1.nextLine());
-		
-		
-		
+			
 		
 		Scanner number2 = new Scanner(System.in);
 		System.out.println("Digite o segundo número: ");
 		int numero2 = Integer.parseInt(number2.nextLine());
 		
-		System.out.println("A soma de " + numero1 + " + "+ numero2 + " = " + numero1+numero2);
+		int soma = numero1 + numero2;
+		
+		System.out.println("A soma de " + numero1 + " + "+ numero2 + " = " + soma);
 		
 		number1.close();
 		number2.close();
