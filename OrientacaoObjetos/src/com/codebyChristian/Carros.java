b7ea0c50fc11de570dad55b3a0a927e5c9aadc64
codebyChristian <Christ.Cardoso.santos@gmail.com>
2024-01-12 15:53:12 -0300

    Métodos acessores e padrão POJO

--- OrientacaoObjetos/src/com/codebyChristian/Carros.java
+++ OrientacaoObjetos/src/com/codebyChristian/Carros.java
@@ -1,17 +1,63 @@
 package com.codebyChristian;
 
 public class Carros {
+
+// Para utilizar o get() e set(), precisamos usar como private
+ 	
+	private int placa;
+	private int quantidadePortas;
+	private String nome;
+	private String chassi;
+	private String marca;
 	
-	public int placa;
-	public int quantidadePortas;
-	public String nome;
-	public String chassi;
-	public String marca;
 	
+	
+	
+	
+	public int getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(int placa) {
+		this.placa = placa;
+	}
+
+	public int getQuantidadePortas() {
+		return quantidadePortas;
+	}
+
+	public void setQuantidadePortas(int quantidadePortas) {
+		this.quantidadePortas = quantidadePortas;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getChassi() {
+		return chassi;
+	}
+
+	public void setChassi(String chassi) {
+		this.chassi = chassi;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
 
 	
 // Métodos
-	
+
+
 	public void ligar() {
 		System.out.println("Carro ligado...");
 		
--- OrientacaoObjetos/src/com/codebyChristian/main/Main.java
+++ OrientacaoObjetos/src/com/codebyChristian/main/Main.java
@@ -5,20 +5,25 @@ import com.codebyChristian.Carros;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+// Alterando valores pelo método set() 
 	
 		Carros ferrari = new Carros();
-		ferrari.marca = "Ferrari 1964";
-		ferrari.nome = "Ferrari";
-		ferrari.placa = 000111;
-		ferrari.quantidadePortas = 2;
+		ferrari.setMarca("Fiat");
+		ferrari.setNome("Toreto");
+		ferrari.setPlaca(111333);
+		ferrari.setQuantidadePortas(2);
+// Acessar dados pelo método get()	
+		System.out.println(ferrari.getMarca());
+		
 		ferrari.ligar();
 		
 		Carros gol = new Carros();
-		gol.marca = "Sei lá";
-		gol.nome = "Gol bolinha";
-		gol.placa = 212132;
-		gol.quantidadePortas = 2;
+		gol.setMarca("Sei lá");
+		gol.setNome("Gol bolinha");
+		gol.setPlaca(212132);
+		gol.setQuantidadePortas(2);
+// Acessar dados pelo método get()	
+		System.out.println(gol.getNome());
 		ferrari.ligar();
 		
 	}
