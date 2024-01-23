3bbf82922d0e61dcc6cf43d365eeb798cc8d975a
codebyChristian <Christ.Cardoso.santos@gmail.com>
2024-01-23 14:53:00 -0300

    Serialização e deserialização

--- OrientacaoObjetos/src/com/codebychristian/java/excecoes/ChassiInvalidoExpection.java
+++ OrientacaoObjetos/src/com/codebychristian/java/excecoes/ChassiInvalidoExpection.java
@@ -2,6 +2,8 @@ package com.codebychristian.java.excecoes;
 
 public class ChassiInvalidoExpection extends Exception {
 
+	private static final long serialVersionUID = -5458953010105828006L;
+
 	public ChassiInvalidoExpection(String chassi) {
 		super(String.format("Esse chassi está incorreto [%s]", chassi));
 	}
