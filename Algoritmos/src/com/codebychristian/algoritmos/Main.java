845ccade9b25ef848522070072f5bafb93c95c1d
codebyChristian <Christ.Cardoso.santos@gmail.com>
2024-02-07 22:10:22 -0300

    Analisando dados atráves de Arrays

--- Algoritmos/src/com/codebychristian/algoritmos/Main.java
+++ Algoritmos/src/com/codebychristian/algoritmos/Main.java
@@ -1,10 +1,27 @@
 package com.codebychristian.algoritmos;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+// Em um espaço de memória há 5 divisões, começando do 0, 1, 2, 3, 4;		
+		int[] numbers = new int[5];
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.println("Adicione um número a lista!");
+			int number = scanner.nextInt();
+			numbers[i] = number;
+		}
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.println(numbers[i]);
+		}
+		
+		scanner.close();
+		
 	}
 
 }
