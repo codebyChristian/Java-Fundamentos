c16a5f8d235a5691505aa1235fb6cfa13c7cec7e
codebyChristian <Christ.Cardoso.santos@gmail.com>
2024-02-12 20:55:41 -0300

    Algoritmo de Busca

--- Algoritmos/src/com/codebychristian/algoritmos/Main.java
+++ Algoritmos/src/com/codebychristian/algoritmos/Main.java
@@ -1,5 +1,6 @@
 package com.codebychristian.algoritmos;
 
+
 import java.util.Scanner;
 
 public class Main {
@@ -16,12 +17,31 @@ public class Main {
 			int number = scanner.nextInt();
 			numbers[i] = number;
 		}
+		// Algoritmo de Busca:
+		System.out.println("---BUSCA LINEAR---");
+		System.out.println("Digite um número para saber a posição na lista: ");
+		int busca = scanner.nextInt();
+		int marcadorResultado = -1;
 		for (int i = 0; i < numbers.length; i++) {
-			System.out.println(numbers[i]);
+			if(numbers[i] == busca) {
+				marcadorResultado = i;
+			} 
 		}
-		
-		scanner.close();
-		
+		if (marcadorResultado < 0) {
+			System.out.println("Número não encontrado na lista.");
+		} else {
+			System.out.println(String.format("O número %d está na posição %d.", busca, marcadorResultado));
+		}		
+		// Fim Algoritmo de Busca:		
+		System.out.println("Vetor");
+		imprimirArray(numbers);
+		scanner.close();		
+	}
+	
+	private static void imprimirArray(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
 	}
 
 }
