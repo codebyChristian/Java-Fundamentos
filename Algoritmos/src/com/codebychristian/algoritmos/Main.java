ba2682459dee47c5ad4997fc185034c4d096e8e2
codebyChristian <Christ.Cardoso.santos@gmail.com>
2024-02-13 20:52:43 -0300

    Analisando: Busca Linear

--- Algoritmos/src/com/codebychristian/algoritmos/Main.java
+++ Algoritmos/src/com/codebychristian/algoritmos/Main.java
@@ -17,23 +17,6 @@ public class Main {
 			int number = scanner.nextInt();
 			numbers[i] = number;
 		}
-		// Algoritmo de Busca:
-		System.out.println("---BUSCA LINEAR---");
-		System.out.println("Digite um número para saber a posição na lista: ");
-		int busca = scanner.nextInt();
-		int marcadorResultado = -1;
-		for (int i = 0; i < numbers.length; i++) {
-			if(numbers[i] == busca) {
-				marcadorResultado = i;
-				break;
-			} 
-		}
-		if (marcadorResultado < 0) {
-			System.out.println("Número não encontrado na lista.");
-		} else {
-			System.out.println(String.format("O número %d está na posição %d.", busca, marcadorResultado));
-		}					
-		// Fim Algoritmo de Busca:
 		
 		// Inicio Ordenação -- SELECTION SORT --
 		// Vetor desodernado
---
+++ Algoritmos/src/com/codebychristian/linear/Main.java
@@ -0,0 +1,37 @@
+package com.codebychristian.linear;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Array with five numbers
+		int[] numbers = new int[5];
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		for(int i = 0; i < numbers.length; i++) {
+			System.out.println("Digite 5 números em seguida!");
+			int userNumbers = scanner.nextInt();
+			numbers[i] = userNumbers;
+		}
+		//Search position
+		System.out.println("Busca linear");
+		System.out.println("Digite um número para descobrir sua posição!");
+		int positionNumber = scanner.nextInt();
+		int markPosition = -1;
+		for(int i = 0; i < numbers.length; i++) {
+			if(numbers[i] == positionNumber) {
+				markPosition = i;
+				break;
+			}
+		}
+		if(markPosition < 0) {
+			System.out.println("Número não encontrado.");
+		} else {
+			System.out.println(String.format("O número %d está na posição %d.", positionNumber, markPosition));
+		}
+		scanner.close();
+	}
+
+}
