f004c3176ed4630334351fb4ba00895fe2364ab2
codebyChristian <Christ.Cardoso.santos@gmail.com>
2024-02-27 15:08:37 -0300

    Adicionando os atributos a classe Cliente

---
+++ springboot/src/main/java/br/com/springboot/model/Cliente.java
@@ -0,0 +1,70 @@
+package br.com.springboot.model;
+
+import java.time.LocalDate;
+
+public class Cliente {
+	
+	// Atributos
+	private Long id;
+	private String nome;
+	private String cpf;
+	private LocalDate dataNascimento;
+	private Sexo sexo;
+	private String telefone;
+	private String email;
+	private boolean ativo;
+	
+	// Metódos Acessores
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public Sexo getSexo() {
+		return sexo;
+	}
+	public void setSexo(Sexo sexo) {
+		this.sexo = sexo;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public boolean isAtivo() {
+		return ativo;
+	}
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+	
+	
+	
+}
