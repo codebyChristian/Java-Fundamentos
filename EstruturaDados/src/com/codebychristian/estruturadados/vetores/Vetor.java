0f32e0db86330c3e7d0208a917642f61ae526a97
codebyChristian <Christ.Cardoso.santos@gmail.com>
2024-03-27 15:25:54 -0300

    Exceção para números inválidos

--- EstruturaDados/src/com/codebychristian/estruturadados/vetores/Vetor.java
+++ EstruturaDados/src/com/codebychristian/estruturadados/vetores/Vetor.java
@@ -3,12 +3,27 @@ package com.codebychristian.estruturadados.vetores;
 public class Vetor<T> {
 	
 	private Object[] elementos;
+	private int posicao;
 	
 	public Vetor(int capacidade) {
 		this.elementos = new Object[capacidade];
+		this.posicao = 0;
+	}
+	
+	public Vetor() {
+		this.elementos = new Object[3];         // 4 indices
+		this.posicao = 0;
+	}
+	
+	public void inserir(T element) {
+		this.elementos[this.posicao] = element;
+		posicao++;
 	}
 	
 	public void inserirArrey(int posicao, T element) {
+		if(posicao < this.elementos.length) {
+			throw new IllegalArgumentException(String.format("Posição inválida [%d]", posicao));
+		}
 		this.elementos[posicao] = element;
 	}
 	
