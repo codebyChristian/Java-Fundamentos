381023f8520d16222ae4996c3834d0d4e74d2afe
codebyChristian <Christ.Cardoso.santos@gmail.com>
2024-04-02 15:26:04 -0300

    Métodos de inserir na lista ligada

--- EstruturaDados/src/com/codebychristian/estruturadados/listaligadas/ListaLigada.java
+++ EstruturaDados/src/com/codebychristian/estruturadados/listaligadas/ListaLigada.java
@@ -9,6 +9,27 @@ public class ListaLigada<T> {
 	public ListaLigada() {
 		this.primeiroNo = null;
 		this.ultimoNO = null;
+		tamanho = 0;
 	}
 
+	public void insere(T elemento) {
+		Celulas<T> novoNo = new Celulas<T>(elemento);
+		if(listaVazia()) {
+			this.primeiroNo = novoNo;
+			this.ultimoNO = novoNo;
+		} else {
+			this.ultimoNO.setProximo(novoNo);
+			this.ultimoNO = novoNo;
+		}
+		this.tamanho++;	
+	}
+	
+	public boolean listaVazia() {
+		return this.tamanho == 0;
+	}
+	
+	public int tamanho() {
+		return this.tamanho;
+	}
+ 	
 }
