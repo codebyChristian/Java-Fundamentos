3e54241fd9b0275f5e110f2430e80f1bca14729e
codebyChristian <Christ.Cardoso.santos@gmail.com>
2024-04-05 04:42:34 -0300

    Métodos de inserir: inicio, meio e fim

--- EstruturaDados/src/com/codebychristian/estruturadados/listaligadas/ListaLigada.java
+++ EstruturaDados/src/com/codebychristian/estruturadados/listaligadas/ListaLigada.java
@@ -1,11 +1,11 @@
 package com.codebychristian.estruturadados.listaligadas;
 
 public class ListaLigada<T> {
-	
+
 	private Celulas<T> primeiroNo;
 	private Celulas<T> ultimoNo;
 	private int tamanho;
-	
+
 	public ListaLigada() {
 		this.primeiroNo = null;
 		this.ultimoNo = null;
@@ -14,49 +14,69 @@ public class ListaLigada<T> {
 
 	public void insere(T elemento) {
 		Celulas<T> novoNo = new Celulas<T>(elemento);
-		if(listaVazia()) {
+		if (listaVazia()) {
 			this.primeiroNo = novoNo;
 			this.ultimoNo = novoNo;
 		} else {
 			this.ultimoNo.setProximo(novoNo);
 			this.ultimoNo = novoNo;
 		}
-		this.tamanho++;	
+		this.tamanho++;
 	}
-	
+
 	public void inserirEm(int posicao, T elemento) {
-		if(posicao >= tamanho()) {
+		if (posicao >= tamanho()) {
 			throw new IllegalArgumentException(String.format("Não é possivel adicionar [%d]", posicao));
 		}
-		Celulas<T> noAnterior = recuperarNo(posicao - 1);
-		Celulas<T> noAtual = recuperarNo(posicao);
-		Celulas<T> novoNo = new Celulas<>(elemento);
-		
+		if (posicao == 0) {
+			Celulas<T> novoNo = new Celulas<T>(elemento);
+			novoNo.setProximo(this.primeiroNo);
+			this.primeiroNo = novoNo;
+		} else if (posicao == this.tamanho - 1) {
+			Celulas<T> novoNo = new Celulas<T>(elemento);
+			this.ultimoNo.setProximo(novoNo);
+			this.ultimoNo = novoNo;
+		} else {
+			Celulas<T> noAnterior = recuperarNo(posicao - 1);
+			Celulas<T> noAtual = recuperarNo(posicao);
+			Celulas<T> novoNo = new Celulas<>(elemento);
+			noAnterior.setProximo(novoNo);
+			novoNo.setProximo(noAtual);
+		}
+		this.tamanho++;
 	}
 	
+	public void inserirPrimeiro(T elemento) {
+		inserirEm(0, elemento);
+	}
+	
+	public void inserirUltimo(T elemento) {
+		inserirEm(tamanho - 1, elemento);
+	}
+
 	public T recuperar(int posicao) {
 		Celulas<T> no = recuperarNo(posicao);
-		if(no != null) {
+		if (no != null) {
 			return no.getElemento();
 		}
 		return null;
 	}
-	
+
 	public boolean listaVazia() {
 		return this.tamanho == 0;
 	}
-	
+
 	public int tamanho() {
 		return this.tamanho;
 	}
-	
+
 	private Celulas<T> recuperarNo(int posicao) {
-		if(posicao >= tamanho()) {
+		if (posicao >= tamanho()) {
 			throw new IllegalArgumentException(String.format("Não é possivel adicionar [%d]", posicao));
 		}
 		Celulas<T> resultado = null;
-		for(int i = 0; i <= posicao; i++) {
-			if(i == 0) {
+		for (int i = 0; i <= posicao; i++) {
+			if (i == 0) {
 				resultado = this.primeiroNo;
 			} else {
 				resultado = resultado.getProximo();
@@ -67,7 +87,7 @@ public class ListaLigada<T> {
 
 	@Override
 	public String toString() {
-		if(listaVazia()) {
+		if (listaVazia()) {
 			return "ListaLigada []";
 		} else {
 			Celulas<T> celulaAtual = this.primeiroNo;
@@ -75,17 +95,17 @@ public class ListaLigada<T> {
 			sb.append("Lista [");
 			sb.append(celulaAtual.getElemento() != null ? celulaAtual.getElemento().toString() : "<NULO>");
 			sb.append(",");
-			while(celulaAtual.getProximo() != null) {
-				sb.append(celulaAtual.getProximo().getElemento() != null ? celulaAtual.getProximo().getElemento().toString() : "<NULO>");
+			while (celulaAtual.getProximo() != null) {
+				sb.append(celulaAtual.getProximo().getElemento() != null
+						? celulaAtual.getProximo().getElemento().toString()
+						: "<NULO>");
 				sb.append(",");
 				celulaAtual = celulaAtual.getProximo();
 			}
 			sb.append("]");
-			return sb.toString();		
+			return sb.toString();
 		}
-		
+
 	}
-	
-	
- 	
+
 }
--- EstruturaDados/src/com/codebychristian/estruturadados/main/Main.java
+++ EstruturaDados/src/com/codebychristian/estruturadados/main/Main.java
@@ -16,10 +16,10 @@ public class Main {
 		System.out.println("3. Lista Ligada");
 		Scanner scanner = new Scanner(System.in);
 		int opcao = scanner.nextInt();
-		
+
 		switch (opcao) {
 		case 1:
-			fazerGerenciamentoMemoria();	
+			fazerGerenciamentoMemoria();
 			break;
 		case 2:
 			fazerVetor();
@@ -28,15 +28,18 @@ public class Main {
 			fazerListaLigada();
 			break;
 		}
-			
+
 		scanner.close();
 	}
-	
+
 	private static void fazerListaLigada() {
 		ListaLigada<Pessoa> listaPessoas = new ListaLigada<Pessoa>();
 		listaPessoas.insere(new Pessoa(1, "Christian"));
 		listaPessoas.insere(new Pessoa(2, "Ernany"));
 		listaPessoas.insere(new Pessoa(3, "Sabad"));
+		listaPessoas.inserirEm(1, new Pessoa(4, "Bit"));
+		listaPessoas.inserirPrimeiro(new Pessoa(5, "Aççougue"));
+		listaPessoas.inserirUltimo(new Pessoa(6, "Gravidade"));
 		System.out.println(listaPessoas.toString());
 	}
 
@@ -49,22 +52,22 @@ public class Main {
 		System.out.println("***********");
 		System.out.println(a);
 		System.out.println(b);
-		System.out.println(a==b);
+		System.out.println(a == b);
 		System.out.println("***********");
 		Pessoa person1 = new Pessoa(0, "Chris");
 		System.out.println(person1.toString());
-		Pessoa person2 = new Pessoa(0, "Chris");   // Cria um novo espaço no Heap
+		Pessoa person2 = new Pessoa(0, "Chris"); // Cria um novo espaço no Heap
 		System.out.println(person2.toString());
 		System.out.println("***********");
-		person2.setNome("Chistian");      
+		person2.setNome("Chistian");
 		System.out.println(person1.toString());
 		System.out.println(person2.toString());
 		System.out.println(person1.equals(person2));
-		
+
 	}
-	
+
 	public static void fazerVetor() {
-		Vetor<Pessoa> vetorPessoa = new Vetor<Pessoa>();   // 0 1 2 3 4
+		Vetor<Pessoa> vetorPessoa = new Vetor<Pessoa>(); // 0 1 2 3 4
 		vetorPessoa.inserir(new Pessoa(1, "CR7 7"));
 		vetorPessoa.inserir(new Pessoa(2, "Chistian 6"));
 		vetorPessoa.inserir(new Pessoa(3, "Maquina 5"));
@@ -76,14 +79,14 @@ public class Main {
 		for (int i = 0; i < vetorPessoa.tamanho(); i++) {
 			System.out.println(vetorPessoa.recuperarArrey(i).getNome());
 		}
-		// recurar no posicao 1,  referencia com indice
+		// recurar no posicao 1, referencia com indice
 		Pessoa p1 = vetorPessoa.recuperarArrey(1);
 		Pessoa indicErrado = new Pessoa(15, "Perido");
 		System.out.println(vetorPessoa.contemParametro(p1));
 		System.out.println(vetorPessoa.contemParametro(indicErrado));
 		System.out.println(vetorPessoa.indice(p1));
 		System.out.println(vetorPessoa.indice(indicErrado));
-		// remover 
+		// remover
 		vetorPessoa.remove(2);
 		System.out.println(vetorPessoa);
 		vetorPessoa.remove(p1);
