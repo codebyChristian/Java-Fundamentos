260960268b10c3ca7b2813590ea84264cdf2abcd
codebyChristian <Christ.Cardoso.santos@gmail.com>
2024-05-17 15:43:37 -0300

    Revisão

--- PrimeiroPrograma/src/Primeiro/Principal.java
+++ PrimeiroPrograma/src/Primeiro/Principal.java
@@ -6,41 +6,42 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-		int idade = 10;
-		String nome = "Christian";
-		idade = idade +2;
-		double peso = 72.5;
+		// nota aluno
+		double nota_aluno = 8;
+		double frequencia_aluno = 75;
+		// regras
+		double nota_aprovacao = 7;
+		double frequencia_necessaria = 75;
 		
-		Scanner teclado = new Scanner(System.in);
-		System.out.println("Digite idade, peso e nome: ");
-		idade = teclado.nextInt();
-		peso = teclado.nextDouble();
-		nome = teclado.next();
-		teclado.close();
-		
-		
-		System.out.println();
-		
-		System.out.println("Nome:" + nome);
-		System.out.printf("Idade: %d,\n", idade);
-		System.out.printf("Peso: %.2f\n",peso);
-		
-		
-		System.out.println("Idade: " + idade);
-		System.out.printf("Idade: %d,\n", idade);
-		System.out.printf("Peso: %.2f\n",peso);
-		System.out.println("Ola Mundo! ");
-		System.out.println("Hello Word!");	
-		
-		if(idade <18) {
-			System.out.println("Criança");
+		if (nota_aluno >= nota_aprovacao && frequencia_aluno >= frequencia_necessaria) {
+			System.out.println("Aprovado!");
+		} else {
+			System.out.println("Não leia teoria!");
 		}
-		else if(idade <65) {
-			System.out.println("Idoso");
-		}
-		else {
-			System.out.println("Muito Idoso");
-		}
+		
+		//
+		
+		  int idade = 10; String nome = "Christian"; idade = idade +2; double peso =
+		  72.5;
+		  
+		  Scanner teclado = new Scanner(System.in);
+		  System.out.println("Digite idade, peso e nome: "); idade = teclado.nextInt();
+		  peso = teclado.nextDouble(); nome = teclado.next(); teclado.close();
+		  
+		  
+		  System.out.println();
+		  
+		  System.out.println("Nome:" + nome); System.out.printf("Idade: %d,\n", idade);
+		  System.out.printf("Peso: %.2f\n",peso);
+		  
+		  
+		  System.out.println("Idade: " + idade); System.out.printf("Idade: %d,\n",
+		  idade); System.out.printf("Peso: %.2f\n",peso);
+		  System.out.println("Ola Mundo! "); System.out.println("Hello Word!");
+		  
+		  if(idade <18) { System.out.println("Criança"); } else if(idade <65) {
+		  System.out.println("Idoso"); } else { System.out.println("Muito Idoso"); }
+		 
 				
 	}	
 		
