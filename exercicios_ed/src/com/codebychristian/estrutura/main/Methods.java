05bf4a074e90cc145e55eedd3570608c992be1f2
codebyChristian <Christ.Cardoso.santos@gmail.com>
2024-05-22 15:29:27 -0300

    Recursividade

--- exercicios_ed/src/com/codebychristian/estrutura/main/Methods.java
+++ exercicios_ed/src/com/codebychristian/estrutura/main/Methods.java
@@ -13,6 +13,22 @@ public class Methods {
 			System.out.println("Entrada proibida.");
 		}
 	}
+	
+	static int somaNumero(int x, int y) {
+		return x + y;
+	}
+	
+	static double somaNumero(double x, double y) {
+		return x + y;
+	}
+	
+	static int sum(int k) {
+		if(k > 0) {
+			return k + sum(k - 1);
+		} else {
+			return 0;
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -20,6 +36,15 @@ public class Methods {
 		myMetodo("2", 2023);
 		
 		checkIdade(22);
+		//soma
+		int z = somaNumero(5, 5);  // recomendado armazenar o retorno da variavel
+		double zz = somaNumero(12.3, 34.2);
+		System.out.println(z);
+		System.out.println(zz);
+		
+		// recursividade
+		int resultado = sum(10);
+		System.out.println(resultado);
 		
 	}
 
