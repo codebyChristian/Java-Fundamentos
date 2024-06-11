8c641e03acba7ed711f88d555eced95e3261f343
codebyChristian <Christ.Cardoso.santos@gmail.com>
2024-06-11 04:20:44 -0300

    Somar itens do Array

---
+++ exercicios_ed/src/com/codebychristian/estrutura/teste/ExercicioArray.java
@@ -0,0 +1,21 @@
+package com.codebychristian.estrutura.teste;
+
+
+
+public class ExercicioArray {
+
+	public static void main(String[] args) {
+		// Soma de numeros de array
+		int[] numberArray = {2, 4, 7, 7};
+		int soma = 0;
+		int i;
+		
+	
+		for(i = 0; i < numberArray.length; i++) {
+			soma += numberArray[i];
+		}
+		System.out.println("A soma do Array é: " + soma);
+
+	}
+
+}
