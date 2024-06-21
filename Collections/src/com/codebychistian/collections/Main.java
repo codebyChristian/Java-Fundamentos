445c8b08a1da9508aee2495e266013512e237c29
codebyChristian <Christ.Cardoso.santos@gmail.com>
2024-06-21 20:24:14 -0300

    Iterator e loop com for

--- Collections/src/com/codebychistian/collections/Main.java
+++ Collections/src/com/codebychistian/collections/Main.java
@@ -1,6 +1,7 @@
 package com.codebychistian.collections;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import com.codebychristian.models.Pessoa;
@@ -13,7 +14,18 @@ public class Main {
 		pessoas.add(new Pessoa(1, "Chris"));
 		pessoas.add(new Pessoa(2, "Japinha"));
 		pessoas.add(new Pessoa(3, "Doquinha"));
-		System.out.println(pessoas.toString());
+		System.out.println("Loop com For:");
+		for (int i = 0; i < pessoas.size(); i++) {
+			Pessoa p = pessoas.get(i);
+			System.out.println(p);
+		}
+		// Iterator
+		System.out.println("Usando iterator:");
+		Iterator<Pessoa> iteratorPessoa = pessoas.iterator();
+		while (iteratorPessoa.hasNext()) {
+			Pessoa p = iteratorPessoa.next();
+			System.out.println(p);
+		}
 
 }
 }
