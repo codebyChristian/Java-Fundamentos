0287bbe0011ba3246ecdf52e7768310c4b3d0d12
codebyChristian <Christ.Cardoso.santos@gmail.com>
2024-07-01 14:33:10 -0300

    AbstractSequentialList

--- Collections/src/com/codebychistian/collections/Main.java
+++ Collections/src/com/codebychistian/collections/Main.java
@@ -1,7 +1,6 @@
 package com.codebychistian.collections;
 
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.codebychristian.models.Pessoa;
@@ -10,7 +9,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// List , ArrayList , Collections podem ser instanciados
-		List<Pessoa> pessoas = new LinkedList<Pessoa>();
+		List<Pessoa> pessoas = new ArrayList<Pessoa>();
 		pessoas.add(new Pessoa(1, "Chris"));
 		pessoas.add(new Pessoa(2, "Japinha"));
 		pessoas.add(new Pessoa(3, "Doquinha"));
