9fec8edfef9d5b16f3f7ccb27143c31676596c40
codebyChristian <Christ.Cardoso.santos@gmail.com>
2024-07-03 15:21:50 -0300

    Set - LinkedHashSet

--- Collections/src/com/codebychistian/collections/MainSet.java
+++ Collections/src/com/codebychistian/collections/MainSet.java
@@ -1,6 +1,7 @@
 package com.codebychistian.collections;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 import com.codebychristian.models.Pessoa;
@@ -8,12 +9,29 @@ import com.codebychristian.models.Pessoa;
 public class MainSet {
 
 	public static void main(String[] args) {
-		// 
+		//  HashSet tem ordem abrirtária, não sequindo ordem de inserção
 		Set<Pessoa> set = new HashSet<Pessoa>();
 		set.add(new Pessoa(1, "Primeiro"));
 		set.add(new Pessoa(2, "Segundo"));
+		set.add(null);
+		//	iterador
+		for (Pessoa p : set) {
+			System.out.println(p);
+		}
 		System.out.println(set);
 		
+		System.out.println("----------------------");
+		// LinkedHashSet mantém a ordem de exerção
+		Set<Pessoa> link = new LinkedHashSet<Pessoa>();
+		link.add(new Pessoa(4, "Quarto"));
+		link.add(new Pessoa(5, "Quinto"));
+		link.add(new Pessoa(6, "Sexto"));
+		link.add(null);
+		// iterator
+		for (Pessoa p : link ) {
+			System.out.println(p);
+		}
+		System.out.println(link);
 
 	}
 
