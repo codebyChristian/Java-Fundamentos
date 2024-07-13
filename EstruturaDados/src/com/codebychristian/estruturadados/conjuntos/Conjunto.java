7a49b1707a9324c1be081fb890af89663d42650e
codebyChristian <Christ.Cardoso.santos@gmail.com>
2024-07-13 15:22:59 -0300

    Entendendo estrutura set - Conjuntos

---
+++ EstruturaDados/src/com/codebychristian/estruturadados/conjuntos/Conjunto.java
@@ -0,0 +1,50 @@
+package com.codebychristian.estruturadados.conjuntos;
+
+public class Conjunto<T> {
+	// Estruturas que não permiti dados duplicados
+	
+	public void insere(T elemento) {
+		
+	}
+	
+	public void insereEm(int posicao, T elemento) {
+		
+	}
+	
+	public void inserePrimeiro() {
+		
+	}
+	
+	public void insereUltimo() {
+		
+	}
+	
+	public void recuperar(int posicao) {
+		
+	}
+	
+	public boolean estaVazio() {
+		return false;
+	}
+	
+	public int tamanho() {
+		return 0;
+	}
+	
+	public boolean contem(T elemento) {
+		return false;
+	}
+	
+	public int indeci(T elemento) {
+		return 0;
+	}
+	
+	public void remove(int posicao) {
+		
+	}
+	
+	public void remove(T elemento) {
+		
+	}
+
+}
