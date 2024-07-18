aa856b53d4642286ab09889ccd8fa5468a020703
codebyChristian <Christ.Cardoso.santos@gmail.com>
2024-07-18 15:18:07 -0300

    Definindo método de inserção na tabela de espalhamento

--- EstruturaDados/src/com/codebychristian/tabelaespanhamento/TabelaEspalhamento.java
+++ EstruturaDados/src/com/codebychristian/tabelaespanhamento/TabelaEspalhamento.java
@@ -7,7 +7,7 @@ public class TabelaEspalhamento<T> {   // tipo: T = generico
 	// 0: p1, p2, p7   --->  Cada elemento terá uma lista ligada
 	// 1: p3, p5, p6   --->  Cada elemento terá uma lista ligada
 	// 3: p4           --->  Cada elemento terá uma lista ligada
-	
+	// por isso ela não terá ordem
 	
 	// Implementando explicação acima
 	private ListaLigada<ListaLigada<T>> elementos;
@@ -21,6 +21,20 @@ public class TabelaEspalhamento<T> {   // tipo: T = generico
 		}
 	}
 	
+	public boolean insere(T elemento) {
+		int numeroEsplamento = this.gerarNumeroEspanhalmento(elemento);
+		ListaLigada<T> categoria = this.elementos.recuperar(numeroEsplamento);
+		// se dentro da categoria o elemento for repetido ele não será salvo
+		if (categoria.contem(elemento) ) {
+			return false;
+		}
+		// insere o elemento sabendo que não há elementos repetidos
+		categoria.insere(elemento);
+		return true;
+	}
 	
+	private int gerarNumeroEspanhalmento(T elemento) {
+		return elemento.hashCode() % 16;
+	}
 	
 }
